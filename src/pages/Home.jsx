0d409dcdf13b03455489c776c9d156fdb1f1ecb6
Collier Hawkins <collier_hawkins@bellsouth.net>
2022-11-08 15:42:22 -0500

    Finish Home Page Movie List - Responsiveness needs work

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,15 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 import Button, { OutlineButton } from '../components/button/Button';
 import Modal, { ModalContent } from '../components/modal/Modal';
+import MovieList from '../components/movie-list/MovieList';
+
+import { category, movieType } from '../api/tmdbApi';
 
 import './home.scss';
 import 'boxicons';
 
-const Home = () => {
 
-  
+const Home = () => {
 
   SwiperCore.use([Autoplay]);
 
@@ -22,6 +24,14 @@ const Home = () => {
     fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=5466b7f9715fde9407c767ec4cfbdb90&language=en-US")
     .then(res => res.json())
     .then(data => setPopularMovies(data.results.slice(0, 10)))
+  }, [])
+
+  const [ trendingMovies, setTrendingMovies ] = useState([])
+
+  useEffect(() => {
+    fetch("https://api.themoviedb.org/3/trending/movie/week?api_key=5466b7f9715fde9407c767ec4cfbdb90&language=en-US")
+    .then(res => res.json())
+    .then(data => setTrendingMovies(data.results.slice(0, 10)))
   }, [])
 
   return (
@@ -45,6 +55,39 @@ const Home = () => {
                     ))
                 }
             </Swiper>
+        </div>
+        <div className="container">
+            <div className="section mb-3">
+                <div className="trending">
+                    <div className="section__header mb-2">
+                        <h2>What's Trending?</h2>
+                        <div className="section__header__ten">
+                            <box-icon name="trending-up" color="white" size="lg" animation="tada"></box-icon>
+                        </div>
+                    </div>
+                        <Swiper
+                            grabCursor={true}
+                            spaceBetween={10}
+                            slidesPerView={'auto'}
+                    >
+                            {
+                                trendingMovies.map((item, i) => (
+                                    <SwiperSlide key={i}>
+                                        <div className="numbers">
+                                            <h1>{i + 1}</h1>
+                                        </div>
+                                        <Link style={{textDecoration:"none",color:"white"}} to={`/movie/${item.id}`} >
+                                            <img src={`https://image.tmdb.org/t/p/w500${item && item.poster_path}`} />
+                                        </Link>
+                                    </SwiperSlide>
+                                ))
+                            }
+                        </Swiper>
+                </div>
+                <div className="back">
+                        <h1>TOP 10</h1>
+                </div>
+            </div>
         </div>
     </>
   );
@@ -80,5 +123,6 @@ const SlideItem = props => {
         </div>
     )
 }
+
 
 export default Home;
